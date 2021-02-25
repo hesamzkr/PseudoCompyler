6308fdd87c85672df569ab25a57cea1344568616
hesamzkr <hesam.zk.81@gmail.com>
2021-02-25 23:39:53 +0100

    first commit

---
+++ out.cs
@@ -0,0 +1,15 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+class Main
+{
+    public static void Main(string[] args)
+    {
+        //
+        //
+        //
+        //
+        // fn()
+        
